8d02d6d058d8493a895f808b2d9bcd45f70e58e8
Nguyen Cong Duy <ncongduy@gmail.com>
2022-02-03 19:27:27 +0200

    assignment 2: creat a method get list name of all countries

--- assignment-2/index.js
+++ assignment-2/index.js
@@ -1,1 +1,20 @@
 // Your code here
+class Countries {
+	constructor() {}
+
+	async getAllCountries() {
+		try {
+			const fetchData = await fetch('https://restcountries.com/v3.1/all');
+			const responseData = await fetchData.json();
+			const getListName = responseData.map(
+				(country) => country.name.official
+			);
+
+			return getListName;
+		} catch (error) {
+			console.log('Error when get all list countries', error);
+		}
+	}
+}
+
+const example = new Countries();

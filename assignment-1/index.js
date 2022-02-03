5b919594c06bdf0824f8abee55110f3ff71a6909
Nguyen Cong Duy <ncongduy@gmail.com>
2022-02-03 19:00:16 +0200

    feat: complete assignment 1

--- assignment-1/index.js
+++ assignment-1/index.js
@@ -1,1 +1,24 @@
 // Your code here
+let number = 1;
+
+function printNumber() {
+	if (number > 1e6) {
+		clearInterval(validInterval);
+		return;
+	} else {
+		const flag = number + 1000;
+		while (number <= 1e6) {
+			console.log(number);
+			number++;
+
+			if (number === flag) break;
+		}
+	}
+}
+
+const validInterval = setInterval(printNumber, 2000);
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', function () {
+	alert('Well done! Browser does not hang.');
+});
